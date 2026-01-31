7919d7181cca1f4242b2b424cce47984de3e1d72
JenniferPylko <3450413+JenniferPylko@users.noreply.github.com>
2026-01-31 03:25:49 -0500

    added versioning

--- scripts/pack.js
+++ scripts/pack.js
@@ -1,4 +1,15 @@
+import { readdir, readFile, writeFile } from "node:fs/promises"
+import process from "node:process"
 import { Zip } from "zip-lib"
+
+const manifest = JSON.parse(await readFile("modrinth.index.json", { encoding: "utf-8" }))
+manifest.versionId = manifest.versionId.replace(/[0-9]*$/, process.env.GITHUB_RUN_NUMBER ?? "0")
+await writeFile("modrinth.index.json", JSON.stringify(manifest, null, 2))
+
+const bcc = await readFile("config/bcc-common.toml", { encoding: "utf-8" })
+await writeFile("config/bcc-common.toml", bcc.replace(/modpackVersion = "\d*\.\d*\.\d*"/g, `modpackVersion = "${manifest.versionId}"`))
+
+await writeFile("version", manifest.versionId)
 
 const modpack = new Zip()
 
