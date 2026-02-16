acb1aa6dfb7d4feb0dce70d3fcb5117eea0f4737
JenniferPylko <3450413+JenniferPylko@users.noreply.github.com>
2026-02-15 20:40:26 -0500

    hopefully final bit of dedicated server working

---
+++ kubejs/server_scripts/fabrishot.js
@@ -0,0 +1,60 @@
+
+
+const $fabrishot = Java.loadClass("me.ramidzkh.fabrishot.Fabrishot")
+
+const fabrishot = () => {
+    $fabrishot.startCapture()
+    while($fabrishot.isInCapture()) {}
+}
+
+ServerEvents.basicCommand("fabrishot", fabrishot)
+
+const $fabrishot_config = Java.loadClass("me.ramidzkh.fabrishot.config.Config")
+
+ServerEvents.basicCommand("panoramashot", ($) => {
+    const configured_filename = $fabrishot_config.CUSTOM_FILE_NAME
+    const configured_hide_hud = $fabrishot_config.HIDE_HUD
+    const configured_width = $fabrishot_config.CAPTURE_WIDTH
+    const configured_height = $fabrishot_config.CAPTURE_HEIGHT
+    $fabrishot_config.CUSTOM_FILE_NAME = "panorama_0"
+    $fabrishot_config.HIDE_HUD = true
+    $fabrishot_config.CAPTURE_WIDTH = 2160
+    $fabrishot_config.CAPTURE_HEIGHT = 2160
+    $.player.runCommand("tick freeze")
+    $.player.runCommand("teleport @p ~ ~ ~ ~ 0")
+    const delay = 500
+    setTimeout(() => {
+        $.player.runCommand("fabrishot")
+        $fabrishot_config.CUSTOM_FILE_NAME = "panorama_1"
+        $.player.runCommand("teleport @p ~ ~ ~ ~90 0")
+        setTimeout(() => {
+            $.player.runCommand("fabrishot")
+            $fabrishot_config.CUSTOM_FILE_NAME = "panorama_2"
+            $.player.runCommand("teleport @p ~ ~ ~ ~90 0")
+            setTimeout(() => {
+                $.player.runCommand("fabrishot")
+                $fabrishot_config.CUSTOM_FILE_NAME = "panorama_3"
+                $.player.runCommand("teleport @p ~ ~ ~ ~90 0")
+                setTimeout(() => {
+                $.player.runCommand("fabrishot")
+                    $fabrishot_config.CUSTOM_FILE_NAME = "panorama_4"
+                    $.player.runCommand("teleport @p ~ ~ ~ ~90 -90")
+                    setTimeout(() => {
+                        $.player.runCommand("fabrishot")
+                        $fabrishot_config.CUSTOM_FILE_NAME = "panorama_5"
+                        $.player.runCommand("teleport @p ~ ~ ~ ~ 90")
+                        setTimeout(() => {
+                            $.player.runCommand("fabrishot")
+                            $fabrishot_config.CUSTOM_FILE_NAME = configured_filename
+                            $fabrishot_config.HIDE_HUD = configured_hide_hud
+                            $fabrishot_config.CAPTURE_WIDTH = configured_width
+                            $fabrishot_config.CAPTURE_HEIGHT = configured_height
+                            $.player.runCommand("teleport @p ~ ~ ~ ~ 0")
+                            $.player.runCommand("tick unfreeze")
+                        }, delay)
+                    }, delay)
+                }, delay)
+            }, delay)
+        }, delay)
+    }, delay)
+})
--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -125,65 +125,6 @@ ServerEvents.recipes(($) => {
     })
 })
 
-const $fabrishot = Java.loadClass("me.ramidzkh.fabrishot.Fabrishot")
-
-const fabrishot = () => {
-    $fabrishot.startCapture()
-    while($fabrishot.isInCapture()) {}
-}
-
-ServerEvents.basicCommand("fabrishot", fabrishot)
-
-const $fabrishot_config = Java.loadClass("me.ramidzkh.fabrishot.config.Config")
-
-ServerEvents.basicCommand("panoramashot", ($) => {
-    const configured_filename = $fabrishot_config.CUSTOM_FILE_NAME
-    const configured_hide_hud = $fabrishot_config.HIDE_HUD
-    const configured_width = $fabrishot_config.CAPTURE_WIDTH
-    const configured_height = $fabrishot_config.CAPTURE_HEIGHT
-    $fabrishot_config.CUSTOM_FILE_NAME = "panorama_0"
-    $fabrishot_config.HIDE_HUD = true
-    $fabrishot_config.CAPTURE_WIDTH = 2160
-    $fabrishot_config.CAPTURE_HEIGHT = 2160
-    $.player.runCommand("tick freeze")
-    $.player.runCommand("teleport @p ~ ~ ~ ~ 0")
-    const delay = 500
-    setTimeout(() => {
-        $.player.runCommand("fabrishot")
-        $fabrishot_config.CUSTOM_FILE_NAME = "panorama_1"
-        $.player.runCommand("teleport @p ~ ~ ~ ~90 0")
-        setTimeout(() => {
-            $.player.runCommand("fabrishot")
-            $fabrishot_config.CUSTOM_FILE_NAME = "panorama_2"
-            $.player.runCommand("teleport @p ~ ~ ~ ~90 0")
-            setTimeout(() => {
-                $.player.runCommand("fabrishot")
-                $fabrishot_config.CUSTOM_FILE_NAME = "panorama_3"
-                $.player.runCommand("teleport @p ~ ~ ~ ~90 0")
-                setTimeout(() => {
-                $.player.runCommand("fabrishot")
-                    $fabrishot_config.CUSTOM_FILE_NAME = "panorama_4"
-                    $.player.runCommand("teleport @p ~ ~ ~ ~90 -90")
-                    setTimeout(() => {
-                        $.player.runCommand("fabrishot")
-                        $fabrishot_config.CUSTOM_FILE_NAME = "panorama_5"
-                        $.player.runCommand("teleport @p ~ ~ ~ ~ 90")
-                        setTimeout(() => {
-                            $.player.runCommand("fabrishot")
-                            $fabrishot_config.CUSTOM_FILE_NAME = configured_filename
-                            $fabrishot_config.HIDE_HUD = configured_hide_hud
-                            $fabrishot_config.CAPTURE_WIDTH = configured_width
-                            $fabrishot_config.CAPTURE_HEIGHT = configured_height
-                            $.player.runCommand("teleport @p ~ ~ ~ ~ 0")
-                            $.player.runCommand("tick unfreeze")
-                        }, delay)
-                    }, delay)
-                }, delay)
-            }, delay)
-        }, delay)
-    }, delay)
-})
-
 BlockEvents.randomTick("minecraft:obsidian", ($) => {
     if (Math.random() < 0.02) {
         const neighbors = [
@@ -270,10 +211,10 @@ PlayerEvents.loggedIn(($) => {
         $.server.runCommandSilent(`give ${$.entity.username} minecraft:stick`)
         $.server.runCommandSilent(`give ${$.entity.username} kubejs:guide`)
         $.server.runCommandSilent(`give ${$.entity.username} sleeping_bags:${sleeping_bag_colors[Math.floor(Math.random() * sleeping_bag_colors.length)]}_sleeping_bag`)
-        //$.server.runCommandSilent(`give ${$.entity.username} wardentools:deep_fruit 16`)
+        $.server.runCommandSilent(`give ${$.entity.username} hardcore_torches:lit_lantern`)
     }
 })
 
 PlayerEvents.respawned(($) => {
-    //$.server.runCommandSilent(`give ${$.entity.username} wardentools:deep_fruit`)
+    $.server.runCommandSilent(`effect give ${$.entity.username} minecraft:glowing 120`)
 })
