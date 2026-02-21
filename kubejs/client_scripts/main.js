dd1f00aede0156f551b91f08f050187e5e980dad
JenniferPylko <3450413+JenniferPylko@users.noreply.github.com>
2026-02-21 15:02:03 -0500

    made the nether fog appear reasonable instead of light gray

--- kubejs/client_scripts/main.js
+++ kubejs/client_scripts/main.js
@@ -1,3 +1,14 @@
 ItemEvents.modifyTooltips(($) => {
     $.add("hardcore_torches:fire_starter", Text.yellow("Long-press interact to ignite blocks"))
 })
+
+ClientEvents.generateAssets("last", ($) => {
+    $.json("c:fog_definitions/tag/biome/is_nether", {
+        colors: {
+            day: "#9f0000",
+            night: "#9f0000"
+        },
+        end_multiplier: 1.0,
+        start_multiplier: 1.0
+    })
+})
